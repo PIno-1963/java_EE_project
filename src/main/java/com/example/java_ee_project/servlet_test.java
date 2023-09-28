e4e87c6aa97f3fd1db5f656de17ee7d1e2f6d09b
hamza <hamzamoussaoui041@gmail.com>
2023-09-28 23:09:46 +0100

    second  version

--- src/main/java/com/example/java_ee_project/servlet_test.java
+++ src/main/java/com/example/java_ee_project/servlet_test.java
@@ -2,8 +2,8 @@ package com.example.java_ee_project;
 
 import java.io.*;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -12,19 +12,10 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class servlet_test extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
-
-        // Hello
-        request.setAttribute("message", message);
-
-        // Forward the request to hello.html
         RequestDispatcher dispatcher = request.getRequestDispatcher("src/main/webapp/index.jsp");
         dispatcher.forward(request, response);
     }
@@ -32,27 +23,17 @@ public class servlet_test extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
-        // Get form data
+
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         String email = request.getParameter("email");
+        String name = firstname +" "+ lastname ;
 
-        // Database connection and insertion code
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // Replace with your database driver
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/form test", "root", "hamza");
-            String sql = "INSERT INTO ident (name, email) VALUES (?, ?)";
-            PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setString(1, firstname);
-            statement.setString(2, lastname);
-            statement.setString(2, email);
-
-            statement.executeUpdate();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (Connection conn = jdbc_conn.getConnection()) {
+            jdbc_insertion.insertData(conn, name, email);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-
 
         request.setAttribute("lastname", lastname);
         request.setAttribute("firstname", firstname);
